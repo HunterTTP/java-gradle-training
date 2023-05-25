e72b4c8517317e80af734ca4c5ee77cb447e5c17
HunterTTP <HunterTTP@gmail.com>
2023-05-25 16:10:18 -0400

    Additional notes

---
+++ src/main/java/ClassesPart1/Car.java
@@ -0,0 +1,57 @@
+package ClassesPart1;
+
+public class Car {
+
+    //fields (since class level, would be variables in method level)
+    private String make = "Tesla";
+    private String model = "Model 3";
+    private String color = "Grey";
+    private int doors = 4;
+    private boolean convertible = false;
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    public void describeCar() {
+
+        System.out.printf("%s - %s - %s - %d", make, model, color, doors);
+    }
+
+ }
---
+++ src/main/java/ClassesPart1/Main.java
@@ -0,0 +1,12 @@
+package ClassesPart1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car myCar = new Car();
+        myCar.describeCar();
+        System.out.printf("\nIs this car convertible? %s", myCar.isConvertible());
+
+    }
+}
